783d4b7adc3d8d74ba6adceb55294965d707689f
4L4N <digitalforce.ac@gmail.com>
2023-09-26 19:24:12 -0600

    corrected incorrect syntax in ifndef; added sum overload

---
+++ Practica_4/Programas/C++/include/fraccion.hpp
@@ -0,0 +1,57 @@
+#ifndef FRACCION_HPP
+#define FRACCION_HPP
+
+class Fraccion{
+    private:
+        int a;
+        int b;
+    public:
+        Fraccion();
+        Fraccion(int a, int b);
+        //Getters
+        int getA();
+        int getB();
+        //Setters
+        void setA(int a);
+        void setB(int b);
+        //Methods
+        std::string toLatex();
+        //Operator Overload
+        // +
+        Fraccion operator + (const Fraccion &otro);
+        Fraccion operator + (int otro);
+        Fraccion operator += (const Fraccion &otro);
+        Fraccion operator += (int otro);
+        Fraccion operator ++ ();
+        // -
+        Fraccion operator - (const Fraccion &otro);
+        Fraccion operator - (int &otro);
+        Fraccion operator -= (const Fraccion &otro);
+        Fraccion operator -= (int &otro);
+        Fraccion operator -- ();
+        // *
+        Fraccion operator * (const Fraccion &otro);
+        Fraccion operator * (int &otro);
+        Fraccion operator *= (const Fraccion &otro);
+        Fraccion operator *= (int &otro);
+        // /
+        Fraccion operator / (const Fraccion &otro);
+        Fraccion operator / (int &otro);
+        Fraccion operator /= (const Fraccion &otro);
+        Fraccion operator /= (int &otro);
+        // order
+        bool operator == (const Fraccion &otro);
+        bool operator == (int &otro);
+        bool operator != (const Fraccion &otro);
+        bool operator != (int &otro);
+        bool operator < (const Fraccion &otro);
+        bool operator < (int &otro);
+        bool operator > (const Fraccion &otro);
+        bool operator > (int &otro);
+        bool operator <= (const Fraccion &otro);
+        bool operator <= (int &otro);
+        bool operator >= (const Fraccion &otro);
+        bool operator >= (int &otro);
+};
+
+#endif
---
+++ Practica_4/Programas/C++/source/fraccion.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "fraccion.hpp"
+
+//Constructors
+Fraccion::Fraccion() {
+    this->a = 0;
+    this->b = 1;
+}
+
+Fraccion::Fraccion(int a, int b) {
+    this->a = a;
+    this->b = b;
+}
+
+//Getters
+int Fraccion::getA()  {
+    return this->a;
+}
+int Fraccion::getB()  {
+    return this->b;
+}
+
+//Setters
+void Fraccion::setA(int a) {
+    this->a = a;
+}
+
+void Fraccion::setB(int b) {
+    this->b = b;
+}
+
+//Methods
+std::string Fraccion::toLatex() {
+    std::string result = "\\frac{" + std::to_string(this->a) + "}{" + std::to_string(this->b) + "}";
+    return result;
+}
+// Oprerator overloading
+// +
+Fraccion Fraccion::operator + (const Fraccion &otro) {
+    int a = this->a * otro.b + otro.a * this->b;
+    int b = this->b * otro.b;
+
+    Fraccion result(a, b);
+
+    return result;
+}
+
+Fraccion Fraccion::operator + (int otro) {
+    Fraccion itof = Fraccion(otro, 1);
+
+    return *this + itof;
+}
+
+Fraccion Fraccion::operator += (const Fraccion &otro) {
+    *this = *this + otro;
+
+    return *this;
+}
+
+Fraccion Fraccion::operator += (int otro) {
+    Fraccion itof = Fraccion(otro, 1);
+
+    *this = *this + itof;
+
+    return *this;
+}
+
+Fraccion Fraccion::operator ++ () {
+    *this += 1;
+
+    return *this;
+}
